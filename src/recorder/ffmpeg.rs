7e110e4faaa8b255a3e88812e0efa76b9cd15883
jkesle <joshua.e.kesler@gmail.com>
2025-11-29 21:00:25 -0600

    add GPL3 license

--- src/recorder/ffmpeg.rs
+++ src/recorder/ffmpeg.rs
@@ -1,3 +1,18 @@
+// Copyright (C) 2025 Joshua Kesler
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 use super::types::{EncoderPreset, EncodingQuality, EncodingSpeed};
 
 pub fn build_cmd(width: u32, height: u32, fps: u32, format: &str, encoder: EncoderPreset, quality: EncodingQuality, speed: EncodingSpeed, filename: &str) -> Vec<String> {
